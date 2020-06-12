e7cd7475250c863f972a32919a5df118fbed71ab
WeiPei2219062260 <2219062260@qq.com>
2020-06-12 10:31:28 +0800

    m这是一个作业

---
+++ MVC_5/demo1/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace demo1.Models
+{
+    public class Student
+    {
+        [Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Height { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
